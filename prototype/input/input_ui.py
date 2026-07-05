2e38deab019b5239f88556b885c60df436c03048
Pang-Chin Wei <pangchinw@gmail.com>
2026-07-06 03:05:38 +0800

    Add Comment

--- prototype/input/input_ui.py
+++ prototype/input/input_ui.py
@@ -121,6 +121,9 @@ def submit_AI(
 # I just saw Rohan's product generation thingy - maybe try to integrate that into this UI so user can use both
 # Or maybe just have the AI straight up generate graaphics into the web UI 
 
+# Should probably also add a box after response for what user would like to change to the generate thingy
+
+
 def build_ui():
     with gr.Blocks(title="Product Prompt Builder") as app:
         gr.Markdown("## Product Prompt Builder\nFill in what you know about the product and how you want the page to feel. Hit Generate when you're ready.")
